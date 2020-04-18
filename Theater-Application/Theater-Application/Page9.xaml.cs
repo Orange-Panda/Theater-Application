398a7814cb777a4a778c98353bc6f358ba968f0d
rshynski <63022504+rshynski@users.noreply.github.com>
2020-04-18 18:13:59 -0400

    Update for Luke

---
+++ Theater-Application/Theater-Application/Page9.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+namespace Theater_Application
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class Page9 : ContentPage
+{
+        private List<Seats> SeatsArrayList;
+
+        public Page9()
+        {
+            InitializeComponent();
+            SeatsArrayList = new List<Seats>();
+            for (int i = 0; i < 50; i++)
+            {
+                SeatsArrayList.Add(new Seats { Name = "" });
+            }
+
+            gridLayout.RowDefinitions.Add(new RowDefinition());
+            gridLayout.RowDefinitions.Add(new RowDefinition());
+            gridLayout.RowDefinitions.Add(new RowDefinition());
+            gridLayout.RowDefinitions.Add(new RowDefinition());
+            gridLayout.RowDefinitions.Add(new RowDefinition());
+            gridLayout.RowDefinitions.Add(new RowDefinition());
+            gridLayout.RowDefinitions.Add(new RowDefinition());
+            gridLayout.RowDefinitions.Add(new RowDefinition());
+            gridLayout.RowDefinitions.Add(new RowDefinition());
+            gridLayout.RowDefinitions.Add(new RowDefinition());
+            gridLayout.ColumnDefinitions.Add(new ColumnDefinition());
+            gridLayout.ColumnDefinitions.Add(new ColumnDefinition());
+            gridLayout.ColumnDefinitions.Add(new ColumnDefinition());
+            gridLayout.ColumnDefinitions.Add(new ColumnDefinition());
+            gridLayout.ColumnDefinitions.Add(new ColumnDefinition());
+
+            var seatIndex = 0;
+            for (int rowIndex = 0; rowIndex <= 9; rowIndex++)
+            {
+                for (int columnIndex = 0; columnIndex <= 5; columnIndex++)
+                {
+                    if (seatIndex >= SeatsArrayList.Count)
+                    {
+                        break;
+                    }
+                    var product = SeatsArrayList[seatIndex];
+                    seatIndex += 1;
+                    var Button = new Button
+                    {
+                        Text = product.Name,
+                        VerticalOptions = LayoutOptions.Center,
+                        HorizontalOptions = LayoutOptions.Center
+                    };
+                    gridLayout.Children.Add(Button, columnIndex, rowIndex);
+                }
+            }
+        }
+    }
+}
---
+++ Theater-Application/Theater-Application/Seat.cs
@@ -0,0 +1,7 @@
+﻿namespace Theater_Application
+{
+    class Seat
+    {
+
+    }
+}
