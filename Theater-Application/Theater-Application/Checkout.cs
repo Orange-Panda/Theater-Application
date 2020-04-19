4883cf82842a2804f53c702f58f2c2f9c3dae442
rshynski <63022504+rshynski@users.noreply.github.com>
2020-04-19 18:07:52 -0400

    UI SetUp for Checkout Page and Structs for Card/Ticket type
    UI is set up as a template where backend code can be filled in to populate needed areas. Event listener will be needed to be set up for the button to send confirmation email with confirmation number and details.
    
    The Checkout.cs page has two structs that can be used for 4 types of tickets and  types of card brands.
    
    The ticket and the card have a selection tool (Picker) so users can select their card and ticket type.

---
+++ Theater-Application/Theater-Application/Checkout.cs
@@ -0,0 +1,22 @@
+﻿namespace Theater_Application
+{
+    public struct Ticket
+    {
+        public  TicketType ticketType;
+    }
+
+    public enum TicketType
+    {
+        Child, Adult, Military, Senior
+    }
+
+    public struct Card
+    {
+        public CardType cardType;
+    }
+
+    public enum CardType
+    {
+        Visa, Mastercard, American_Express, Discover
+    }
+}
---
+++ Theater-Application/Theater-Application/Checkout.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Theater_Application
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+public partial class Checkout : ContentPage
+{
+    public Checkout()
+    {
+           
+     }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
